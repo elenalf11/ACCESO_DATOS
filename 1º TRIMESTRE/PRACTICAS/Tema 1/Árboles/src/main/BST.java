51d698238aed9ec5003400a4eada38aa7e69f100
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2024-10-04 19:49:03 +0200

    Arbol con funcion delete y recorridos implementados

--- 1º TRIMESTRE/PRACTICAS/Tema 1/Árboles/src/main/BST.java
+++ 1º TRIMESTRE/PRACTICAS/Tema 1/Árboles/src/main/BST.java
@@ -75,4 +75,129 @@ public class BST {
 
 	}
 
+	/**
+	 * Metodo para eliminar elementos de los nodos
+	 * 
+	 * @param element es el elemento que se desea eliminar
+	 */
+	public void delete(int element) {
+		this.root = deleteRecursive(this.root, element);
+	}
+
+	/**
+	 * Metodo para eliminar elementos de los nodos de manera recursiva comprobando
+	 * los criterios de los arboles binarios de busqueda
+	 * 
+	 * @param current es el nodo en que inicia la busqueda
+	 * @param element es el elemento que se desea eliminar
+	 * @return devuelve el nodo que se va a eliminar
+	 */
+	private Node deleteRecursive(Node current, int element) {
+		// El nodo esta vacio
+		if (current == null) {
+			return null;
+		}
+		// El valor del nodo a eliminar se ha encontrado
+		if (element == current.value) {
+			// Caso 1: El nodo es un nodo hoja
+			if (current.left == null && current.right == null) {
+				return null;
+			}
+			// Caso 2: El nodo solo tiene un hijo
+			if (current.right == null) {
+				return current.left;
+			}
+
+			if (current.left == null) {
+				return current.right;
+			}
+
+			// Caso 3: El nodo tiene 2 hijos
+			int smallestValue = findSmallestValue(current.right);
+			System.out.println(smallestValue);
+			current.value = smallestValue;
+			current.right = deleteRecursive(current.right, smallestValue);
+		}
+		// El valor es menor que el valor del nodo actual
+		if (element < current.value) {
+			current.left = deleteRecursive(current.left, element);
+			return current;
+		} else {
+			// El valor es mayor que el valor del nodo actual
+			current.right = deleteRecursive(current.right, element);
+			return current;
+		}
+	}
+
+	/**
+	 * Metodo que usaremos para buscar el nodo mas pequenio del subarbol derecho
+	 * 
+	 * @param root es el nodo raiz
+	 * @return devuelve el valor mas pequenio del subarbol derecho
+	 */
+	private int findSmallestValue(Node root) {
+		return root.left == null ? root.value : findSmallestValue(root.left);
+
+	}
+
+	/**
+	 * Metodo que imprime el arbol en Pre-Orden
+	 */
+	public void PreOrden() {
+		PreOrdenRecursive(this.root);
+	}
+
+	/**
+	 * Metodo que imprime el arbol en Pre-Orden de manera recursiva
+	 * 
+	 * @param current es el nodo en el que inicia el recorrido
+	 */
+	private void PreOrdenRecursive(Node current) {
+		if (current != null) {
+			System.out.println("Valor: " + current.value);
+			PreOrdenRecursive(current.left);
+			PreOrdenRecursive(current.right);
+		}
+	}
+
+	/**
+	 * Metodo que imprime el arbol en Post-Orden
+	 */
+	public void PostOrden() {
+		PostOrdenRecursive(this.root);
+	}
+
+	/**
+	 * Metodo que imprime el arbol en Post-Orden de manera recursiva
+	 * 
+	 * @param current es el nodo en el que inicia el recorrido
+	 */
+	private void PostOrdenRecursive(Node current) {
+		if (current != null) {
+			PostOrdenRecursive(current.left);
+			PostOrdenRecursive(current.right);
+			System.out.println(current.value);
+		}
+	}
+
+	/**
+	 * Metodo que imprime el arbol en In-Orden
+	 */
+	public void InOrden() {
+		InOrdenRecursive(this.root);
+	}
+
+	/**
+	 * Metodo que imprime el arbol en In-Orden de manera recursiva
+	 * 
+	 * @param current es el nodo en el que inicia el recorrido
+	 */
+	private void InOrdenRecursive(Node current) {
+		if (current != null) {
+			InOrdenRecursive(current.left);
+			System.out.println(current.value);
+			InOrdenRecursive(current.right);
+		}
+	}
+
 }
