6effa376fa92979790c3f6085628e291e1d11a19
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2024-10-14 11:20:10 +0200

    Ejemplo 1 Excepciones

--- 1º TRIMESTRE/PRACTICAS/Tema 2/Example_1/src/main/Main.java
+++ 1º TRIMESTRE/PRACTICAS/Tema 2/Example_1/src/main/Main.java
@@ -2,10 +2,22 @@ package main;
 
 public class Main {
 
-	public static void main(String[] args) throws ExceptionDivisionByZero, ExceptionNegativeNumber {
-		Arithmetic prueba = new Arithmetic(2, 0);
-		int z = prueba.division();
-		System.out.println(z);
+	public static void main(String[] args){
+		try {
+			Arithmetic prueba = new Arithmetic(-2, 0);
+			int z = prueba.division();
+			System.out.println(z);
+		}
+		catch(ExceptionDivisionByZero e) {
+			System.out.println(e.getMessage());
+		}
+		catch(ExceptionNegativeNumber e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			System.out.println("La ejecucion ha acabado");
+		}
+		
 
 	}
 
